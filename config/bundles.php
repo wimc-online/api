30454bab3f9e3143d546319168964648d9c42011
Jan Malisz <mistyfiky@gmail.com>
2020-06-29 00:02:13 +0200

    Add jwt bundle

--- config/bundles.php
+++ config/bundles.php
@@ -7,9 +7,10 @@ $bundles = [
     Nelmio\CorsBundle\NelmioCorsBundle::class => ['all' => true],
     Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => ['all' => true],
     ApiPlatform\Core\Bridge\Symfony\Bundle\ApiPlatformBundle::class => ['all' => true],
+    Lexik\Bundle\JWTAuthenticationBundle\LexikJWTAuthenticationBundle::class => ['all' => true],
 ];
 
-if ('dev' === $this->getEnvironment()) {
+if ('dev' === getenv('APP_ENV')) {
     $bundles[Symfony\Bundle\MakerBundle\MakerBundle::class] = ['dev' => true];
 }
 
