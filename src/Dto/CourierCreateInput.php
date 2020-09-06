a252162bee98995e11779bf8be09a03c08991125
Jan Malisz <mistyfiky@gmail.com>
2020-09-07 00:57:25 +0200

    Change creating user in keycloak order in data persister

--- src/Dto/CourierCreateInput.php
+++ src/Dto/CourierCreateInput.php
@@ -2,6 +2,26 @@
 
 namespace App\Dto;
 
+use Symfony\Component\Validator\Constraints as Assert;
+
 final class CourierCreateInput
 {
+    /**
+     * @var string
+     * @Assert\NotBlank
+     * @Assert\Email
+     */
+    public $email;
+
+    /**
+     * @var string
+     * @Assert\NotBlank
+     */
+    public $firstName;
+
+    /**
+     * @var string
+     * @Assert\NotBlank
+     */
+    public $lastName;
 }
--- src/Entity/Courier.php
+++ src/Entity/Courier.php
@@ -5,6 +5,7 @@ namespace App\Entity;
 use ApiPlatform\Core\Annotation\ApiFilter;
 use ApiPlatform\Core\Annotation\ApiResource;
 use ApiPlatform\Core\Annotation\ApiSubresource;
+use App\Controller\PostCourier;
 use App\Dto\CourierCreateInput as CreateInput;
 use App\Dto\CourierOutput as Output;
 use App\Filter\ActiveCourierFilter;
@@ -37,6 +38,21 @@ class Courier
     private $id;
 
     /**
+     * @var string
+     */
+    private $email;
+
+    /**
+     * @var string
+     */
+    private $firstName;
+
+    /**
+     * @var string
+     */
+    private $lastName;
+
+    /**
      * @ORM\OneToMany(targetEntity=Position::class, mappedBy="courier", orphanRemoval=true)
      */
     private $positions;
@@ -61,6 +77,42 @@ class Courier
     public function setId(string $id): self
     {
         $this->id = $id;
+
+        return $this;
+    }
+
+    public function getEmail(): string
+    {
+        return $this->email;
+    }
+
+    public function setEmail(string $email): self
+    {
+        $this->email = $email;
+
+        return $this;
+    }
+
+    public function getFirstName(): string
+    {
+        return $this->firstName;
+    }
+
+    public function setFirstName(string $firstName): self
+    {
+        $this->firstName = $firstName;
+
+        return $this;
+    }
+
+    public function getLastName(): string
+    {
+        return $this->lastName;
+    }
+
+    public function setLastName(string $lastName): self
+    {
+        $this->lastName = $lastName;
 
         return $this;
     }
