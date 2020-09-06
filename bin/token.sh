d5e5d235c9845777c5959706ffe6c2d764f6f9f2
Jan Malisz <mistyfiky@gmail.com>
2020-09-06 20:11:56 +0200

    Fix token fetching script

--- bin/token.sh
+++ bin/token.sh
@@ -3,17 +3,11 @@
 AUTH_DOMAIN=auth.wimc.online
 AUTH_REALM_NAME=wimc.localhost
 AUTH_CLIENT_ID=api.wimc.localhost
-API_USERNAME=courier
-stty -echo
->&2 printf 'Password: '
-read API_PASSWORD
-stty echo
->&2 printf '\n'
+AUTH_CLIENT_SECRET=073c6e58-7fd2-407d-be45-033447e25386
 API_TOKEN=$(curl -sS -X POST "https://$AUTH_DOMAIN/auth/realms/$AUTH_REALM_NAME/protocol/openid-connect/token" \
  -H "Content-Type: application/x-www-form-urlencoded" \
+ -d "grant_type=client_credentials" \
  -d "client_id=$AUTH_CLIENT_ID" \
- -d "username=$API_USERNAME" \
- -d "password=$API_PASSWORD" \
- -d "grant_type=password" \
+ -d "client_secret=$AUTH_CLIENT_SECRET" \
  | jq -r ".access_token")
 printf 'Bearer %s' "$API_TOKEN"
